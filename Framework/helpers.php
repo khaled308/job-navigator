b1991674d4e58cedca365f4b0568e31cdd0d4303
khaled308 <khaledgaballa4@gmail.com>
2023-12-19 01:34:06 +0200

    setup structure

---
+++ Framework/helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+function loadView($view, $data = []) {
+    extract($data);
+    $path = __DIR__ . "/../views/$view.php";
+    require_once $path;
+}
+
+function redirect($path) {
+    header("Location: $path");
+    exit;
+}
+
+function dd($data) {
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';
+    die;
+}
+
+function old($name) {
+    return $_REQUEST[$name] ?? '';
+}
+
