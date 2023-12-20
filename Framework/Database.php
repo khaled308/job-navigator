86e7efe121506880b4f0f09d67286cb38e5de288
khaled308 <khaledgaballa4@gmail.com>
2023-12-20 04:43:45 +0200

    Add database connection and query methods to Database class

--- Framework/Database.php
+++ Framework/Database.php
@@ -4,5 +4,41 @@ namespace Framework;
 
 class Database
 {
-     
+    private $connection;
+    private static $instance;
+
+    private function __construct()
+    {
+        $dsn = 'mysql:host=' . Config::DB_HOST . ';port=' . Config::DB_PORT . ';dbname=' . Config::DB_NAME;
+        $options = [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+        ];
+
+        try {
+            $this->connection = new \PDO($dsn, Config::DB_USER, Config::DB_PASSWORD, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            $instance = new Database();
+        }
+
+        return $instance;
+    }
+
+    public function query(string $sql, array $params = [])
+    {
+        try {
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute($params);
+            return $stmt;
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
 }
---
+++ Framework/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Framework;
+
+class Config {
+    public const DB_HOST = 'localhost';
+    public const DB_PORT = '3306';
+    public const DB_NAME = 'job_navigator';
+    public const DB_USER = 'user';
+    public const DB_PASSWORD = '123456';
+}
